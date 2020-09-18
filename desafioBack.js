b7a65c35ab996ba58dedb2e74449fefece2afa98
EdmarAJr <trovador@hotmail.com.br>
2020-09-18 18:47:12 -0300

    Adicionar e atualizar carrinho

--- desafioBack.js
+++ desafioBack.js
@@ -5,10 +5,28 @@ const server = new Koa();
 
 server.use(bodyparser());
 
+const formatarSucesso = (ctx, dados, status = 200) => {
+    ctx.status = status;
+    ctx.body = {
+        status: 'sucesso',
+        dados: dados,
+    };
+};
+
+const formatarErro = (ctx, mensagem, status = 404) => {
+    ctx.status = status;
+    ctx.body = {
+        status: "erro",
+        dados: { 
+            mensagem: mensagem,
+        },
+    }; 
+};
+
 const produto = {
-    id:'',
-    nome:'',
-    quantidadeDisponivelnode:'',
+    id:"123",
+    nome:"",
+    quantidadeDisponivel:"",
     valor: 0000,
     deletado: false,
 };
@@ -16,6 +34,127 @@ const produto = {
 const pedido = [];
 pedido.push(produto); /*como o produto vai ser adicionado*/
 
+/*(crud) Create Read Update Delete */
+
+//Métodos HTTP
+const rotasProdutos = (ctx) => {
+    switch (ctx.method) {
+        case "GET":
+            break;
+        case "POST":
+            break;
+        case "PUT":
+            break;
+        case "DELETE":
+            break;
+        default:
+            //ERR0
+            break;
+    }
+};
+
+const obterPedido = () => {
+    return pedido;
+};
+
+const adicionarProduto = (ctx) => {
+    const body = ctx.request.body;
+
+    if (!body.nome || !body.quantidadeDisponivel || !body.valor) {
+        formatarErro(ctx, "Produto indisponível", 400);
+        return;
+    }
+
+    const produto = {
+        id: "", //ver pq id não gera automaticamente
+        nome: body.nome,
+        quantidadeDisponivel: body.quantidadeDisponivel,
+        valor: body.valor,
+        deletado: false,
+    };
+
+    pedido.push(produto);
+    
+    return produto;
+};
+
+const atualizarCarrinho = (ctx) => {
+    const id = ctx.url.split("/")[2];
+    const body = ctx.request.body;
+    if (!body.nome && !body.quantidadeDisponivel && !body.valor) {
+        formatarErro(ctx, "Seu carrinho está vazio", 400);
+    }
+    if (id) {
+        const produtoAtual = pedido[id];
+        if (produtoAtual) {
+            const pedidoAtualizado = {
+                id: Number (id),
+                nome: body.nome ? body.nome : produtoAtual.nome,
+                quantidadeDisponivel: body.quantidadeDisponivel ? body.quantidadeDisponivel : produtoAtual.quantidadeDisponivel,
+                valor: body.valor ? body.valor : produtoAtual.valor,
+                deletado: produtoAtual.deletado,
+            };
+            pedido[id - 1] = pedidoAtualizado;
+
+            return pedidoAtualizado;
+        }    
+    } else {
+        formatarErro(ctx, "Seu carrinho está vazio", 400);
+    }
+};
+
+const rotasPedidos = (ctx) => {
+    switch (ctx.method) {
+        case "GET":
+            const pedido = obterPedido();
+            formatarSucesso(ctx, pedido);
+            break;
+        case "POST":
+            const produto = adicionarProduto(ctx);
+            if (produto) {
+                formatarSucesso(ctx, produto, 201);
+            }  
+            break;
+        case "PUT":
+            const pedidoAtualizado = atualizarCarrinho(ctx);
+
+            if (pedidoAtualizado) {
+                formatarSucesso(ctx, pedidoAtualizado, 200);
+            }
+            break;
+        case "DELETE":
+            break;
+        default:
+            formatarErro(ctx, "Produto não disponível Teste1", 405)
+            break;
+    }
+};
+
+
+const rotas = (ctx) => {
+    const path = ctx.url.split("/"); // ["", "produto / pedido", "1"]
+
+    if (path[1] === "produto") {
+        rotasProdutos(ctx);
+    } else if (path[1] === "pedido") {
+        rotasPedidos(ctx);
+    } else {
+        //erro
+    }
+};
+
+
+
+server.use((ctx) => {
+    rotas(ctx);
+    //ctx.body = "Rodando...";
+});
+
+server.listen(8081, () => console.log("Servidor rodando..."));
+
+
+
+/*
 /*Listar todos os produtos
 Obter informações de um produto em particular
 Adicionar um novo produto
@@ -29,45 +168,4 @@ Obter todos os produtos	GET /orders
 Atualizar estado do pedido	PUT /orders/:id
 Deletar um pedido	DELETE /orders/:id
 Adicionar produtos na lista de produtos de um pedido	PUT /orders/:id
-
-*/
-
-/*(crud)
-create
-read
-update
-delete
-*/
-
-//Métodos HTTP
-const rotas = (ctx) => {
-    switch (ctx.method) {
-        case 'GET':
-            //
-            break;
-        case 'POST':
-            //
-            break;
-        case 'PUT':
-            //
-        break;
-    case 'DELETE':
-            //
-        break;
-    default:
-        //ERRR0
-            break;
-    } 
-}
-
-server.use(ctx => {
-    rotas(ctx);
-    //ctx.body = 'Rodando...';
-});
-
-server.listen(8081, () => console.log("Servidor rodando..."));
-
-
-
-/*
 */
