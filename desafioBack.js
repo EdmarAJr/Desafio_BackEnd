29e17c320a47adb7017fd25c6671ba08f76345d8
EdmarAJr <trovador@hotmail.com.br>
2020-09-19 11:42:46 -0300

    Modificando variáveis

--- desafioBack.js
+++ desafioBack.js
@@ -1,3 +1,4 @@
+//-------------------------------------------------------------não mexer está pronto-----------------------------------------------------------
 const Koa = require('koa');
 const bodyparser = require('koa-bodyparser');
 
@@ -22,42 +23,29 @@ const formatarErro = (ctx, mensagem, status = 404) => {
         },
     }; 
 };
+//-------------------------------------------------------------não mexer está pronto-----------------------------------------------------------
 
+//apagar depois
 const produto = {
     id:"123",
     nome:"",
     quantidadeDisponivel:"",
     valor: 0000,
     deletado: false,
-};
+};/*apagar depois*/
 
-const pedido = [];
-pedido.push(produto); /*como o produto vai ser adicionado*/
+const produtos = [];
+
+/*apagar depois*/produtos.push(produto); /*como o produto vai ser adicionado*/ 
 
 /*(crud) Create Read Update Delete */
-
+//-------------------------------------------------------------não mexer Produto está pronto-----------------------------------------------------------
 //Métodos HTTP
-const rotasProdutos = (ctx) => {
-    switch (ctx.method) {
-        case "GET":
-            break;
-        case "POST":
-            break;
-        case "PUT":
-            break;
-        case "DELETE":
-            break;
-        default:
-            //ERR0
-            break;
-    }
+const obterProdutos = () => {
+    return produtos.filter((produto) => !produto.deletado);
 };
 
-const obterPedido = () => {
-    return pedido;
-};
-
-const adicionarProduto = (ctx) => {
+const adicionarProdutos = (ctx) => {
     const body = ctx.request.body;
 
     if (!body.nome || !body.quantidadeDisponivel || !body.valor) {
@@ -66,56 +54,56 @@ const adicionarProduto = (ctx) => {
     }
 
     const produto = {
-        id: pedido.length + 1,
+        id: produtos.length + 1,
         nome: body.nome,
         quantidadeDisponivel: body.quantidadeDisponivel,
         valor: body.valor,
         deletado: false,
     };
 
-    pedido.push(produto);
+    produtos.push(produto);
     
     return produto;
 };
 
-const atualizarCarrinho = (ctx) => {
+const atualizarProdutos = (ctx) => {
     const id = ctx.url.split("/")[2];
     const body = ctx.request.body;
     if (!body.nome && !body.quantidadeDisponivel && !body.valor) {
-        formatarErro(ctx, "Seu carrinho está vazio", 400);
+        formatarErro(ctx, "Pedido mal-formatado(função atualizarProdutos)", 400);
     }
     if (id) {
-        const produtoAtual = pedido[id - 1]; //conferir depois
+        const produtoAtual = produtos[id - 1];
         if (produtoAtual) {
-            const pedidoAtualizado = {
+            const produtosAtualizado = {
                 id: Number (id),
                 nome: body.nome ? body.nome : produtoAtual.nome,
                 quantidadeDisponivel: body.quantidadeDisponivel ? body.quantidadeDisponivel : produtoAtual.quantidadeDisponivel,
                 valor: body.valor ? body.valor : produtoAtual.valor,
                 deletado: produtoAtual.deletado,
             };
-            pedido[id - 1] = pedidoAtualizado;
+            pedido[id - 1] = produtosAtualizado;
 
-            return pedidoAtualizado;
+            return produtosAtualizado;
         }    
     } else {
-        formatarErro(ctx, "Seu carrinho está vazio", 400);
+        formatarErro(ctx, "Pedido mal-formatado(função atualizarProdutos)", 400);
     }
 };
 
-const deletarProdutoDoCarrinho = (ctx) => {
+const deletarProdutoDeProdutos = (ctx) => {
     const id = ctx.url.split("/")[2];
     const body = ctx.request.body;
     
     if (typeof body.estado !== 'boolean') {
-        formatarErro(ctx, "Pedido mal-formatado", 400);
+        formatarErro(ctx, "Pedido mal-formatado(função deletarProdutoDeProdutos)", 400);
         return;
     }
 
     if (id) {
-        const produtoAtual = pedido[id - 1];
+        const produtoAtual = produtos[id - 1];
         if(produtoAtual) {
-            const pedidoAtualizado = {
+            const produtosAtualizado = {
                 id: produtoAtual.id,
                 nome: produtoAtual.nome,
                 quantidadeDisponivel: produtoAtual.quantidadeDisponivel,
@@ -123,12 +111,135 @@ const deletarProdutoDoCarrinho = (ctx) => {
                 deletado: body.estado,
             };
            
-            pedido[id - 1] = pedidoAtualizado;
+            pedido[id - 1] = produtosAtualizado;
 
-            return pedidoAtualizado;
+            return produtosAtualizado;
         }
     }
 };
+
+const rotasProdutos = (ctx, path) => {
+    switch (ctx.method) {
+        case "GET":
+            const id = path[2];
+            if (id) {
+                const produtoAtual = produtos[id - 1];
+                if (produtoAtual) {
+                    formatarSucesso(ctx, produtoAtual);
+                } else {
+                    formatarErro(ctx, "Produto não encontrado (caso GET rotasProdutos)", 404)
+                }
+            } else {
+                const produtos = obterProdutos();
+                formatarSucesso(ctx, produtos);
+            }
+            break;
+        case "POST":
+            const produto = adicionarProdutos(ctx);
+            if (produto) {
+                formatarSucesso(ctx, produto, 201);
+            }  
+            break;
+        case "PUT":
+            const produtosAtualizado = atualizarProdutos(ctx);
+
+            if (produtosAtualizado) {
+                formatarSucesso(ctx, produtosAtualizado, 200);
+            }
+            break;
+        case "DELETE":
+            const produtoDeletado = deletarProdutoDeProdutos(ctx);
+            
+            if (produtoDeletado) {
+                formatarSucesso(ctx, produtoDeletado, 200);
+            }
+            break;
+        default:
+            formatarErro(ctx, "Pedido mal-formatado (caso default produto deletado rotasProdutos)", 405)
+            break;
+    }
+};
+//-------------------------------------------------------------não mexer Produto está pronto-----------------------------------------------------------
+
+//configurar pedidos 
+
+const obterPedido = () => {
+    return produtos.filter((produto) => !produto.deletado);
+};
+
+const adicionarPedido = (ctx) => {
+    const body = ctx.request.body;
+
+    if (!body.nome || !body.quantidadeDisponivel || !body.valor) {
+        formatarErro(ctx, "Produto indisponível", 400);
+        return;
+    }
+
+    const produto = {
+        id: produtos.length + 1,
+        nome: body.nome,
+        quantidadeDisponivel: body.quantidadeDisponivel,
+        valor: body.valor,
+        deletado: false,
+    };
+
+    produtos.push(produto);
+    
+    return produto;
+};
+
+const atualizarPedido = (ctx) => {
+    const id = ctx.url.split("/")[2];
+    const body = ctx.request.body;
+    if (!body.nome && !body.quantidadeDisponivel && !body.valor) {
+        formatarErro(ctx, "Pedido mal-formatado(função atualizarProdutos)", 400);
+    }
+    if (id) {
+        const produtoAtual = produtos[id - 1];
+        if (produtoAtual) {
+            const produtosAtualizado = {
+                id: Number (id),
+                nome: body.nome ? body.nome : produtoAtual.nome,
+                quantidadeDisponivel: body.quantidadeDisponivel ? body.quantidadeDisponivel : produtoAtual.quantidadeDisponivel,
+                valor: body.valor ? body.valor : produtoAtual.valor,
+                deletado: produtoAtual.deletado,
+            };
+            pedido[id - 1] = produtosAtualizado;
+
+            return produtosAtualizado;
+        }    
+    } else {
+        formatarErro(ctx, "Pedido mal-formatado(função atualizarProdutos)", 400);
+    }
+};
+
+const deletarPedidoDePedidos = (ctx) => {
+    const id = ctx.url.split("/")[2];
+    const body = ctx.request.body;
+    
+    if (typeof body.estado !== 'boolean') {
+        formatarErro(ctx, "Pedido mal-formatado(função deletarProdutoDeProdutos)", 400);
+        return;
+    }
+
+    if (id) {
+        const produtoAtual = produtos[id - 1];
+        if(produtoAtual) {
+            const produtosAtualizado = {
+                id: produtoAtual.id,
+                nome: produtoAtual.nome,
+                quantidadeDisponivel: produtoAtual.quantidadeDisponivel,
+                valor: produtoAtual.valor,
+                deletado: body.estado,
+            };
+           
+            pedido[id - 1] = produtosAtualizado;
+
+            return produtosAtualizado;
+        }
+    }
+};
+
 
 const rotasPedidos = (ctx, path) => {
     switch (ctx.method) {
@@ -139,7 +250,7 @@ const rotasPedidos = (ctx, path) => {
                 if (produtoAtual) {
                     formatarSucesso(ctx, produtoAtual);
                 } else {
-                    formatarErro(ctx, "Carrinho não encontrado", 404)
+                    formatarErro(ctx, "Produto não encontrado (caso GET rotasPedido)", 404)
                 }
             } else {
                 const pedido = obterPedido();
@@ -167,24 +278,23 @@ const rotasPedidos = (ctx, path) => {
             }
             break;
         default:
-            formatarErro(ctx, "Produto não disponível Teste1", 405)
+            formatarErro(ctx, "Pedido mal-formatado (caso default produto deletado rotasPedido)", 405)
             break;
     }
 };
 
-
+//-------------------------------------------------------------não mexer está pronto-----------------------------------------------------------
 const rotas = (ctx) => {
     const path = ctx.url.split("/"); // ["", "produto / pedido", "1"]
 
     if (path[1] === "produto") {
-        rotasProdutos(ctx);
+        rotasProdutos(ctx, path);
     } else if (path[1] === "pedido") {
         rotasPedidos(ctx, path);
     } else {
-        //erro
+        formatarErro(ctx, 'Opção inválida (rotas -no final)', 404)
     }
 };
-
 
 
 server.use((ctx) => {
@@ -193,21 +303,4 @@ server.use((ctx) => {
 });
 
 server.listen(8081, () => console.log("Servidor rodando..."));
-
-
-
-/*
-/*Listar todos os produtos
-Obter informações de um produto em particular
-Adicionar um novo produto
-Atualizar informações de um produto
-Marcar um produto como deletado
-Alterar a quantidade disponível de um produto
-
-Criar um novo pedido	POST /orders
-Obter informações de um pedido em particular	GET /orders/:id
-Obter todos os produtos	GET /orders
-Atualizar estado do pedido	PUT /orders/:id
-Deletar um pedido	DELETE /orders/:id
-Adicionar produtos na lista de produtos de um pedido	PUT /orders/:id
-*/
+//-------------------------------------------------------------não mexer está pronto-----------------------------------------------------------
